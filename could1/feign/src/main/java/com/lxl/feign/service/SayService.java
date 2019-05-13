97330f43ccef5b765ca457e3c805b237727f2284
663ai <aiwanren@yeah.net>
2019-05-13 11:46:15 +0800

    this is second commit

---
+++ could1/feign/src/main/java/com/lxl/feign/service/SayService.java
@@ -0,0 +1,14 @@
+package com.lxl.feign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@FeignClient(value = "client")
+public interface SayService {
+
+    @RequestMapping(value = "/info",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
